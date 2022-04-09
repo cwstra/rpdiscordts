dc16387a9fc98ffcdc615c9aa93142560dbf24e6
Corin (CWStra) <cwstra@gmail.com>
2022-04-09 16:01:38 -0400

    Better parsing

--- bot/src/helpers/string.test.ts
+++ bot/src/helpers/string.test.ts
@@ -1,5 +1,5 @@
 import * as fc from "fast-check";
-import { extractCharacters } from "./string";
+import { extractCharacters, parseAttributes } from "./string";
 
 const stringWithout = (
   excluded: string[],
@@ -22,7 +22,8 @@ describe("extractCharacters", () => {
             stringWithout(["$", ":"])
           )
         ),
-        (firstStr, rest) => {
+        fc.context(),
+        (firstStr, rest, ctx) => {
           const fullStr =
             firstStr +
             rest
@@ -31,6 +32,7 @@ describe("extractCharacters", () => {
               )
               .join("");
           const result = extractCharacters(fullStr, ["$"], [":"]);
+          ctx.log(fullStr);
           expect(result).toEqual(rest.map(([c]) => c));
         }
       )
@@ -39,26 +41,33 @@ describe("extractCharacters", () => {
     fc.assert(
       fc.property(
         fc
-          .set(fc.string({ minLength: 1 }), { minLength: 2, maxLength: 20 })
+          .set(stringWithout([" "], { minLength: 1 }), {
+            minLength: 2,
+            maxLength: 20,
+          })
           .chain((mixedSymbols) => {
             const filtered = mixedSymbols.filter((s1) =>
               mixedSymbols.every((s2) => s1 === s2 || !s2.includes(s1))
             );
-            const strGen = stringWithout(filtered);
+            const sanitized = filtered.length < 2 ? ["$", ":"] : filtered;
+            const strGen = stringWithout(sanitized, { minLength: 1 });
             return fc
-              .integer({ min: 1, max: filtered.length - 1 })
-              .chain((splitIndex) => {
-                const signs = filtered.slice(0, splitIndex);
-                const seps = filtered.slice(splitIndex);
-                return fc.tuple(
-                  fc.constant(signs),
-                  fc.constant(seps),
-                  strGen,
-                  fc.array(fc.tuple(character(strGen), strGen))
-                );
-              });
+              .integer({ min: 1, max: sanitized.length - 1 })
+              .map((splitIndex) => ({
+                signs: sanitized.slice(0, splitIndex),
+                seps: sanitized.slice(splitIndex),
+              }))
+              .chain(({ signs, seps }) =>
+                fc.record({
+                  signs: fc.constant(signs),
+                  seps: fc.constant(seps),
+                  firstStr: strGen,
+                  rest: fc.array(fc.tuple(character(strGen), strGen)),
+                })
+              );
           }),
-        ([signs, seps, firstStr, rest]) => {
+        fc.context(),
+        ({ signs, seps, firstStr, rest }, ctx) => {
           const fullStr =
             firstStr +
             rest
@@ -71,9 +80,36 @@ describe("extractCharacters", () => {
                   }${nextStr}`
               )
               .join("");
+          ctx.log(fullStr);
           const result = extractCharacters(fullStr, signs, seps);
           expect(result).toEqual(rest.map(([c]) => c));
         }
       )
+    ));
+});
+
+const re = /["\s=\\]/;
+const printStr = (str: string) =>
+  re.test(str) ? `"${str.replace(/\\/g, "\\\\").replace(/"/g, '\\"')}"` : str;
+const printDictionary = (dict: Record<string, string | null>) =>
+  Object.entries(dict)
+    .map(([key, value]) =>
+      value === null ? printStr(key) : `${printStr(key)}=${printStr(value)}`
+    )
+    .join(" ");
+
+describe("parseAttributes", () => {
+  it("undoes printDictionary", () =>
+    fc.assert(
+      fc.property(
+        fc.dictionary(fc.string({ minLength: 1 }), fc.string({ minLength: 1 })),
+        fc.context(),
+        (dict, ctx) => {
+          const printed = printDictionary(dict);
+          ctx.log(printed);
+          expect(parseAttributes(printed)).toEqual(dict);
+        }
+      ),
+      { numRuns: 1000 }
     ));
 });
--- bot/src/helpers/string.ts
+++ bot/src/helpers/string.ts
@@ -1,6 +1,17 @@
 import { zipWith } from "fp-ts/lib/Array";
-import { splitEvery } from "rambda";
+import { fromPairs, splitEvery } from "rambda";
 import { last } from "./array";
+import {
+  alt,
+  apply,
+  rep_sc,
+  tok as genTok,
+  seq,
+  buildLexer,
+  list_sc,
+  expectSingleResult,
+  expectEOF,
+} from "typescript-parsec";
 
 const reEscape = (s: string) => s.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 
@@ -60,3 +71,52 @@ export const trimmedMultiLine = (
   ].join("");
   return wholeString.replace(/\s*\n\s*/g, (s) => s.replace(/[^\n]/g, ""));
 };
+
+export const parseAttributes = (() => {
+  type TokenKind = "simple" | "escaped" | "quote" | "eq" | "space";
+  const lexer = buildLexer<TokenKind>([
+    [true, /^[^\\"=\s]+/g, "simple"],
+    [true, /^\\./g, "escaped"],
+    [true, /^"/g, "quote"],
+    [true, /^=/g, "eq"],
+    [true, /^\s+/g, "space"],
+  ]);
+  const tok = (toMatch: TokenKind) => genTok(toMatch);
+  const simple = apply(tok("simple"), (t) => t.text);
+  const escaped = apply(tok("escaped"), (t) =>
+    ['"', "\\"].includes(t.text[1]) ? t.text[1] : t.text
+  );
+  const quoted = apply(
+    seq(
+      tok("quote"),
+      apply(
+        rep_sc(
+          alt(
+            simple,
+            escaped,
+            apply(tok("eq"), () => "="),
+            apply(tok("space"), (s) => s.text)
+          )
+        ),
+        (strs) => strs.join("")
+      ),
+      tok("quote")
+    ),
+    ([, str]) => str
+  );
+  const identifier = alt(quoted, simple);
+  const pair = apply(
+    seq(identifier, tok("eq"), identifier),
+    ([name, , value]): [string, string | null] => [name, value]
+  );
+  const single = apply(identifier, (id): [string, string | null] => [id, null]);
+  const sequence = apply(list_sc(alt(pair, single), tok("space")), (pairs) =>
+    fromPairs(pairs)
+  );
+  return (expr: string): Record<string, string | null> => {
+    const trimmed = expr.trim();
+    return trimmed
+      ? expectSingleResult(expectEOF(sequence.parse(lexer.parse(trimmed))))
+      : {};
+  };
+})();
